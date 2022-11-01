afd697097f8d80543035e6e45fb4659ee54219c0
LeonLin <l1156389201@163.com>
2022-11-01 19:55:21 +0800

    完善基本设置，引入组件库和Promise插件

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // app.js
+import { promisifyAll } from 'miniprogram-api-promise'
+const wxp = wx.p = {}
+// 调用promisifyAll方法将wx这个对象上的异步API promise化后挂载到wxp上
+// 并将这个对象赋值给wx的自定义属性 wx.p，后续通过wx.p来调用promise化后的异步API
+// 如 wx.p.request({})
+promisifyAll(wx, wxp)
+
 App({
   onLaunch() {
     // 展示本地存储能力
